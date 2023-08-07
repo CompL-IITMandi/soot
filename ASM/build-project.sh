03da0459286c1c4f0cb8a9bb3546a443691b7418
arjunhkumar <s21008@students.iitmandi.ac.in>
2023-08-07 20:02:03 +0530

    Adding ASM for workflow

--- ASM/build-project.sh
+++ ASM/build-project.sh
@@ -7,6 +7,6 @@
 # gradle jar
 
 #import asm and asm-tree jar into maven local repos. Masquerade the newly built artifacts as version 9.4 in maven repo because soot using 9.4 in it's build setup.
-mvn install:install-file -Dfile=asm-9.6-SNAPSHOT.jar -DgroupId=org.ow2.asm -DartifactId=asm -Dversion=9.4 -Dpackaging=jar
-mvn install:install-file -Dfile=asm-tree-9.6-SNAPSHOT.jar -DgroupId=org.ow2.asm -DartifactId=asm-tree -Dversion=9.4 -Dpackaging=jar
+mvn install:install-file -Dfile=ASM/asm-9.6-SNAPSHOT.jar -DgroupId=org.ow2.asm -DartifactId=asm -Dversion=9.4 -Dpackaging=jar
+mvn install:install-file -Dfile=ASM/asm-tree-9.6-SNAPSHOT.jar -DgroupId=org.ow2.asm -DartifactId=asm-tree -Dversion=9.4 -Dpackaging=jar
 
